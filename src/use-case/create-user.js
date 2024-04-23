3021521aaf30ab460d22c23e33dc8dbbc8efa277
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-04-23 00:04:15 -0300

    feat: Add CreateUserUseCase

---
+++ src/use-case/create-user.js
@@ -0,0 +1,23 @@
+import { v4 as uuidV4 } from "uuid"
+import bcrypt from "bcrypt"
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user"
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    // TODO: Verificar se o email ja existe
+
+
+    const userId = uuidV4()
+
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    }
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository()
+    return await postgresCreateUserRepository.execute(user)
+  }
+}
