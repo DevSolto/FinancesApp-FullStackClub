6d346f59fcacdbe191cc7c061082e8ef4429fa42
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-04-25 03:44:15 -0300

    feat: Add CreateUserController and validations

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,41 +1,53 @@
+import validator from "validator"
 import { CreateUserUseCase } from "../use-case/create-user.js"
+import { badRequest, created, serverError } from "./helpers.js"
 
 export class CreateUserController {
   async execute(httpRequest) {
     try {
       const params = httpRequest.body
 
-      const requiredFields = ["first_name", "last_name", "email", "password"]
+      const requiredFields = [
+        "first_name", 
+        "last_name", 
+        "email", 
+        "password"
+      ]
 
       for (const field in requiredFields) {
         if (!params[requiredFields[field]] || params[requiredFields[field]].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing param: ${requiredFields[field]}`
-            }
-          }
+          return badRequest({
+            message:`Missing param: ${requiredFields[field]}`
+          })
         }
+      }
+
+      const passwordIsValide = params.password.length > 6
+
+      if(!passwordIsValide){
+        return badRequest({
+          message:"Password must be at least 6 characters."
+        })
+      }
+
+      const emailIsValid = validator.isEmail(params.email)
+
+      if (!emailIsValid){
+        return badRequest({
+          message:"Invalide email. Please provide a valide one."
+        })
       }
 
       const createUserUseCase = new CreateUserUseCase()
 
       const createdUser = await createUserUseCase.execute(params)
 
-      return {
-        statusCode: 201,
-        body: {
-          errorMessage: createdUser
-        }
-      }
+      return created(createdUser)
+
     } catch (error) {
+
       console.log(error)
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: "Internal server error"
-        }
-      }
+      return serverError()
     }
   }
 }
